2885b7d6c65b991818cd457d52c30b996fbb5f6f
geng yue <2677406151@qq.com>
2026-03-27 10:30:04 +0800

    feat: 实现推箱子核心规则

---
+++ src/codex_sokoban_tui/levels.py
@@ -0,0 +1,14 @@
+"""Built-in Sokoban levels."""
+
+BUILTIN_LEVELS: list[tuple[str, ...]] = [
+    (
+        "#####",
+        "#@$.#",
+        "#####",
+    ),
+    (
+        "#####",
+        "#. @$",
+        "#####",
+    ),
+]
